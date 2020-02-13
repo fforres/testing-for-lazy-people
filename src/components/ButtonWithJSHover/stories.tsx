6b813355a31b307e5367daec9ea0820495175d83
fforres <felipe.torressepulveda@gmail.com>
2020-02-13 10:20:26 -0800

    removing stories

--- src/components/ButtonWithJSHover/stories.tsx
+++ src/components/ButtonWithJSHover/stories.tsx
@@ -7,12 +7,12 @@ export default {
   component: Button
 };
 
-export const WithText: React.FC = () => <Button>Hello Button</Button>;
+// export const WithText: React.FC = () => <Button>Hello Button</Button>;
 
-export const WithEmojisAndActions: React.FC = () => (
-  <Button onClick={action("clicked")} onHover={action("hovered")}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-);
+// export const WithEmojisAndActions: React.FC = () => (
+//   <Button onClick={action("clicked")} onHover={action("hovered")}>
+//     <span role="img" aria-label="so cool">
+//       😀 😎 👍 💯
+//     </span>
+//   </Button>
+// );
